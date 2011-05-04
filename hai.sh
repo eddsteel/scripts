d68c5e9b61cbaddf3f414342d7a0a770473f1ccc
Edd Steel <edward.steel@gmail.com>
2011-05-04 18:57:51 +0100

    use whichever of card or origin is accessible

--- hai.sh
+++ hai.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 #
 
-remote=master
+remote=""
 
-if [ -d /mnt/thumb/repo ]; then
-	remote=card
+if ping -w 2  google.com > /dev/null 2>&1; then
+	remote=master
 fi
 
-for d in  ~/.homesick/repos/*;
-do cd $d; git pull $remote master; done
+if [ -d /mnt/thumb/repo ]; then
+	remote="$remote card"
+fi
+
+for d in  ~/.homesick/repos/*; do
+	for r in $remote; do
+		cd $d; git pull $remote master;
+	done
+done
