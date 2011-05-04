79e359708b5f99b33238205d61b1907cdfdc38d9
Edd Steel <edward.steel@gmail.com>
2011-05-04 19:03:07 +0100

    Test each remote, and pull from acessible ones.

--- hai.sh
+++ hai.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 #
+# Updates homesick repos from any available remotes.
+#
+# Fails on first merge conflict.
+#
 
+test_remotes="master card"
 remote=""
 
-if ping -w 2  google.com > /dev/null 2>&1; then
-	remote=master
-fi
-
-if [ -d /mnt/thumb/repo ]; then
-	remote="$remote card"
-fi
+for r in $test_remotes; do
+	if git fetch $r > /dev/null 2>&1; then
+		remote="$remote $r"
+	else
+		echo "Can't reach $r."
+	fi
+done
 
 for d in  ~/.homesick/repos/*; do
 	for r in $remote; do
