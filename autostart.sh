236eda23b0be522de0a076f637b5e113f27600c2
Edd Steel <edward.steel@gmail.com>
2012-01-25 22:51:57 -0800

    minor adjustment of when path is set

--- autostart.sh
+++ autostart.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+export PATH=/home/edd/bin:$PATH
 /usr/bin/autokey &
 /usr/bin/xcompmgr &
-export PATH=/home/edd/bin:$PATH
 
 xrdb -merge .Xresources
 xmodmap .Xmodmap
+
 (sleep 4 && /home/edd/bin/tmon) &
 (sleep 4 && /home/edd/bin/tmutt) &
