c7baf99df4ecbe8c45e3ccacbd07704a8a807e86
Edd Steel <edward.steel@gmail.com>
2012-04-26 22:37:44 -0700

    check connectivity

--- safeguard.sh
+++ safeguard.sh
@@ -2,6 +2,8 @@
 # Test for remote file. If it's there, open a tunnel back here.
 #
 
-if wget -q http://static.eddandkrista.com/sshreverse; then 
-	ssh -N -R 2900:localhost:22 ec2
+if ping -c 1 -w 5 static.eddandkrista.com >/dev/null 2>&1; then
+	if wget -q http://static.eddandkrista.com/sshreverse; then 
+		ssh -N -R 2900:localhost:22 ec2
+	fi
 fi
