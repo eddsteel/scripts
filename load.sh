f6b69f8074cb252c82dc7044d16953e6e1f9c2d4
Edd Steel <edward.steel@gmail.com>
2012-10-03 22:49:28 -0700

    load avg script

---
+++ load.sh
@@ -0,0 +1,13 @@
+#/usr/bin/bash
+
+sys=$(uname -s)
+
+case $sys in
+	Linux)
+		cat /proc/loadavg | cut -f1-3 -d' '
+		;;
+	Darwin)
+		w | head -n 1 | cut -f 4 -d:
+		;;
+	*) echo "0.0 0.0 0.0" ;;
+esac
