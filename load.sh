6240a69d9a9148e762e7614d3223e2df75a75353
Edd Steel <edward.steel@gmail.com>
2012-10-08 22:53:07 -0700

    improved tmux scripts

--- load.sh
+++ load.sh
@@ -4,10 +4,10 @@ sys=$(uname -s)
 
 case $sys in
 	Linux)
-		cat /proc/loadavg | cut -f1-3 -d' '
+		echo " $(cat /proc/loadavg | cut -f1-3 -d' ')"
 		;;
 	Darwin)
 		w | head -n 1 | cut -f 4 -d:
 		;;
-	*) echo "0.0 0.0 0.0" ;;
+	*) echo " 0.0 0.0 0.0" ;;
 esac
