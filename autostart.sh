ea0c9abaa317a3045b1f3bb0b7dd291b21bacf98
Edd Steel <edward.steel@gmail.com>
2013-04-06 21:00:32 -0700

    up

--- autostart.sh
+++ autostart.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 export PATH=/home/edd/bin:$PATH
-/usr/bin/autokey &
 /usr/bin/xcompmgr &
 /usr/bin/sparkleshare start &
+/usr/bin/redshift &
 
-if [ -f ~/.fehbg ]; then source ~/.fehbg; fi
+if [ -f ~/.fehbg ]; then . ~/.fehbg; fi
+if [ -f ~/.screenlayout/normal.sh ]; then $(~/.screenlayout/normal.sh); fi
 
 xset -b
 xrdb -merge .Xresources
