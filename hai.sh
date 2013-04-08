4e43af9a7a7c855a6844de2c9b9e1d28e55a8d76
Edd Steel <edward.steel@hootsuite.com>
2013-04-08 16:36:40 -0700

    quieter, better tags, calendar on initial run

--- hai.sh
+++ hai.sh
@@ -5,6 +5,7 @@
 
 ${initial:=true}
 . ~/.tmux.d/include/functions
+. ~/.zshfunctions/tmux
 
 
 build_chromium_session() {
@@ -18,19 +19,20 @@ build_chromium_session() {
 
 if [ $initial = true ]; then
 	echo "Hai!"
-	pgrep gpg-agent && killall gpg-agent
-	eval $(gpg-agent --daemon)
+	pgrep gpg-agent >/dev/null && killall gpg-agent
+	eval $(gpg-agent --daemon) >/dev/null
 
-	if which kinit; then
+	if which kinit >/dev/null; then
 		pass -c kinit
 		pbpaste | kinit --password-file=STDIN edward
 	fi
 
 	if [ -f ~/.autostart ]; then
-		cat ~/.autostart | while read app; do 
+		cat ~/.autostart | while read app; do
 			nohup $app &
 		done
 	fi
+
 fi
 
 echo "-> Building tmux session"
@@ -44,7 +46,7 @@ if [ $initial = true ]; then
 	build_chromium_session
 
 	echo "-> rebuilding tags"
-	/usr/local/bin/ctags -R -f ~/.tags ~/Projects&
+	find Projects -type d -depth 2 -exec tag-dir '{}' \;
 
 	echo "-> mr up"
 	mr -qi up
@@ -53,5 +55,9 @@ if [ $initial = true ]; then
 	cd ~/Projects; mr -qi up; cd -
 fi
 
+if [ $initial = true ]; then
+	rem
+	read
+fi
 tmux -2 attach -t tmux
 
