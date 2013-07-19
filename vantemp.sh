20b9be157f0d7d09fc218d7ca649def8d343bd8c
Edd Steel <edward.steel@hootsuite.com>
2013-07-19 11:47:18 -0700

    do less on hai, fix vantemp

--- vantemp.sh
+++ vantemp.sh
@@ -6,23 +6,27 @@
 check=true
 
 if [ -f ~/.lasttemp ]; then
-	latest=$(cat .lasttemp | sed -e 's/^\(.*\)@.*/\1/')
+	latest=$(cat ~/.lasttemp | sed -e 's/^\(.*\)@.*/\1/')
 
-	thn=$(date -d "$latest" +'%s')
+	thn=$(date -j -f '%s' "$latest" +'%s')
 	now=$(date +'%s')
 	threshold=600
-	diff=$(echo "$thn - $now" | bc)
+	diff=$(expr $now - $thn)
+
+	echo "$diff <> $threshold" >&2
 
 	if [ $diff -gt $threshold ]; then check=true; else check=false; fi
 fi
 
 if $check; then
-	temp=$(curl http://weather.noaa.gov/weather/current/CYVR.html 2>/dev/null | grep Temperature -A2 -m1 | tail -n 1 | sed -e 's/.*(\(.*\)).*/\1/' -e 's/ /°/' | tr -d '[:space:]')
+	temp=$(curl http://weather.noaa.gov/weather/current/CYVR.html 2>/dev/null | \
+		grep Temperature -A2 -m2 | tail -n 3 | head -n 1 | \
+		sed -e 's/.*(\(.*\)).*/\1/' -e 's/ /°/' | tr -d '[:space:]')
 
-	echo "$(date)@$temp" > .lasttemp
+	echo "$(date +'%s')@$temp" > ~/.lasttemp
 	echo $temp
 else
-	cat .lasttemp | sed -e 's/.*@\(.*\)$/\1/'
+	cat ~/.lasttemp | sed -e 's/.*@\(.*\)$/\1/'
 fi
 
 
