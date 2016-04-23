9fa8f1f7a6ce083ba8205eba1d39658f43926c86
Edd Steel <edward.steel@gmail.com>
2016-04-23 16:43:53 -0700

    don't remove files

--- s3-backup.sh
+++ s3-backup.sh
@@ -2,5 +2,5 @@
 #
 # incremental backup to S3
 
-s3cmd sync ~/Documents/ s3://eddsteel-backup/Documents/ --follow-symlinks --delete-removed
-s3cmd sync ~/Photos/ s3://eddsteel-backup/Photos/ --follow-symlinks --delete-removed
+s3cmd sync ~/Documents/ s3://eddsteel-backup/Documents/ --follow-symlinks 
+s3cmd sync ~/Photos/ s3://eddsteel-backup/Photos/ --follow-symlinks 
