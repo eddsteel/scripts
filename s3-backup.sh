d95344027bc2092e658c91992c183c690d1a9d0e
Edd Steel <edward.steel@gmail.com>
2017-03-11 13:16:05 -0800

    backup phone backup

--- s3-backup.sh
+++ s3-backup.sh
@@ -2,5 +2,4 @@
 #
 # incremental backup to S3
 
-s3cmd sync ~/Documents/ s3://eddsteel-backup/Documents/ --follow-symlinks 
-s3cmd sync ~/Photos/ s3://eddsteel-backup/Photos/ --follow-symlinks 
+s3cmd sync ~/var/phone-backup s3://eddsteel-backup/phone/ --follow-symlinks
