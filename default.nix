8e034530f6edd697d99cbd8438fefae2c01d77ec
Edd Steel <edd@eddsteel.com>
2021-12-03 13:52:56 -0800

    nixify, add s3

---
+++ default.nix
@@ -0,0 +1,17 @@
+{}:
+with (import <nixpkgs> {});
+let
+  include = [
+    ./edit-selection.sh
+    ./gpl
+    ./myip
+    ./s3
+    ./vantemp.sh
+  ];
+in runCommand "scripts" {inherit include;} ''
+mkdir -p $out/bin
+for n in $include; do
+  script=$(echo $n | cut -c 45-) # there must be a better way.
+  cp $n "$out/bin/$script"
+done
+''
